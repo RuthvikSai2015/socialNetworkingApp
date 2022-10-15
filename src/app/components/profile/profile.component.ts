0aaa56fc543281398f9c17ade99041dbe407b486
RuthvikSai2015 <84248537+RuthvikSai2015@users.noreply.github.com>
2022-10-16 00:55:21 +0530

    changes

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,7 +1,7 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
-import {HttpClient} from "@angular/common/http";
-import { DataService} from "../../services/data.service";
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
+import { DataService } from "../../services/data.service";
 
 @Component({
   selector: 'app-profile',
@@ -17,7 +17,7 @@ export class ProfileComponent implements OnInit {
   zipCheck: any;
   passwordCheck: any;
   userName: string | undefined;
-  displayName : string | undefined
+  displayName: string | undefined
   emailId: string | undefined
   contactNumber: string | undefined
   dateOfBirth: string | undefined
@@ -30,6 +30,7 @@ export class ProfileComponent implements OnInit {
   ziper: any;
   img: File | undefined;
   imgUrl: string | undefined;
+  successMessage: string | undefined;
   // fileToUpload: File | null = null;
   constructor(private http: HttpClient, private router: Router, private data: DataService, private userData: DataService) {
     this.nameCheck = "";
@@ -37,19 +38,20 @@ export class ProfileComponent implements OnInit {
     this.birthCheck = "";
     this.zipCheck = "";
     this.phoneCheck = "";
-    this.passwordCheck="";
-    this.data.currentMessage.subscribe(res=>this.userName=res);
+    this.passwordCheck = "";
+    this.data.currentMessage.subscribe(res => this.userName = res);
     this.url = "";
+    this.successMessage = "";
   }
   ngOnInit(): void {
-     this.imgUrl = "https://m.media-amazon.com/images/I/71zIISn3b5S._SX425_.jpg";
-      this.userName = localStorage.getItem('userName')?.toString();
-      this.DOB = <string>localStorage?.getItem('dateOfBirth');
-      this.phone = <string>localStorage?.getItem('phone');
-      this.ziper = <string>localStorage?.getItem('ZIP');
-      this.displayName = <string>localStorage?.getItem('displayName');
+    this.imgUrl = "https://m.media-amazon.com/images/I/71zIISn3b5S._SX425_.jpg";
+    this.userName = localStorage.getItem('userName')?.toString();
+    this.DOB = <string>localStorage?.getItem('dateOfBirth');
+    this.phone = <string>localStorage?.getItem('phone');
+    this.ziper = <string>localStorage?.getItem('ZIP');
+    this.displayName = <string>localStorage?.getItem('displayName');
     //  this.Password = <string>localStorage?.getItem(('password'));
-      this.email = <string>localStorage?.getItem(('email'));
+    this.email = <string>localStorage?.getItem(('email'));
 
   }
 
@@ -58,7 +60,7 @@ export class ProfileComponent implements OnInit {
   }
 
   onSubmit2() {
-var namePattern =  new RegExp("^[A-Za-z]\\w{3,29}$");
+    var namePattern = new RegExp("^[A-Za-z]\\w{3,29}$");
     var emailPattern = new RegExp(".@.")
     var zipPattern = new RegExp("[0-9]{5}")
     var phonePattern = new RegExp("[0-9]{3}-[0-9]{3}-[0-9]{4}")
@@ -75,55 +77,72 @@ var namePattern =  new RegExp("^[A-Za-z]\\w{3,29}$");
     var dobday = parseInt(ymd[2], 10);
     var userDob = new Date();
     userDob.setFullYear(dobyear, dobmonth - 1, dobday);
-   if(typeof this.dateOfBirth === "string"){
-    this.birthCheck = "Please enter Date of Birth";
-   }
-      
+    if (typeof this.dateOfBirth === "string") {
+      this.birthCheck = "Please enter Date of Birth";
+    }
+
     if (userDob.getTime() > now) {
       this.birthCheck = "User must older than 18 years old";
     }
     else {
       this.birthCheck = "";
     }
-    if (typeof this.userName === "string" && this.userName != "")
-      resultName = namePattern.test(this.userName);
+    if (this.userName != '') {
+      if (typeof this.userName === "string")
+        if (this.userName != '') {
+          resultName = namePattern.test(this.userName);
+        }
+    }
+
     if (!resultName)
       this.nameCheck = "Please enter a valid value that should not start with digit and min length is 6";
 
-    if (this.userName && resultName)
+    if (this.userName && resultName && this.userName != "")
       this.nameCheck = "";
-    if (typeof this.emailId === "string" && this.emailId != "")
+    if (this.emailId) {
       resultEmail = emailPattern.test(this.emailId);
-    if (!resultEmail)
-      this.emailCheck = "Invalid Email Address! Enter a valid Email Address of the form xxxxx@xxxxx.xxx";
+      if (!resultEmail)
+        this.emailCheck = "Invalid Email Address! Enter a valid Email Address of the form xxxxx@xxxxx.xxx";
+    }
     if (this.emailId && resultEmail)
       this.emailCheck = "";
-    if (typeof this.zipCode === "string")
+    if (this.zipCode) {
       resultZIP = zipPattern.test(this.zipCode);
-    if (!resultZIP)
-      this.zipCheck = "Invalid Zipcode! Enter a 5-digit Zip Code.";
-    if ((<HTMLInputElement>document.getElementById("zipCode")).value.length != 5)
-      this.zipCheck = "Invalid Zipcode! Enter a 5-digit Zip Code.";
+      if (!resultZIP)
+        this.zipCheck = "Invalid Zipcode! Enter a 5-digit Zip Code.";
+      if ((<HTMLInputElement>document.getElementById("zipCode")).value.length != 5)
+        this.zipCheck = "Invalid Zipcode! Enter a 5-digit Zip Code.";
+    }
+
     if (this.zipCode && resultZIP && this.zipCode.length == 5)
       this.zipCheck = "";
-    if (typeof this.contactNumber === "string")
+    if (this.zipCode && resultZIP && this.zipCode.length == 5)
+      this.zipCheck = "";
+    if (this.contactNumber) {
       resultPhone = phonePattern.test(this.contactNumber);
-    if (!resultPhone)
-      this.phoneCheck = "Invalid Phone Number! Enter a 10 digit phone number in the form 123-123-1234";
+      if (!resultPhone)
+        this.phoneCheck = "Invalid Phone Number! Enter a 10 digit phone number in the form 123-123-1234";
+    }
+
     if (this.contactNumber && resultPhone)
       this.phoneCheck = "";
-    if ((<HTMLInputElement>document.getElementById("password")).value.length == 0)
-      this.passwordCheck = "The password cannot be empty";
-    else if (this.Password == this.confirmPassword)
-      this.passwordCheck="";
-    else
-      this.passwordCheck = "Passwords do not match!";
+    if (this.Password) {
+      if ((<HTMLInputElement>document.getElementById("password")).value.length == 0)
+        this.passwordCheck = "The password cannot be empty";
+      else if (this.Password == this.confirmPassword)
+        this.passwordCheck = "";
+      else
+        this.passwordCheck = "Passwords do not match!";
+    }
+
     if (this.birthCheck == "" &&
       this.nameCheck == "" &&
       this.emailCheck == "" &&
       this.zipCheck == "" &&
       this.phoneCheck == "" &&
-      this.passwordCheck=="") {
+      this.passwordCheck == "") {
+      this.successMessage = "User Details Updated Succesfully!";
+      localStorage.setItem("userName", <string>this.userName);
       this.router.navigate(['profile']);
     }
   }
