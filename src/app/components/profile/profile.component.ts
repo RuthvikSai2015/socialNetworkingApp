cea4a1c04e853fc8b324010583f442d26e34dd8e
RuthvikSai2015 <84248537+RuthvikSai2015@users.noreply.github.com>
2022-10-16 00:29:49 +0530

    Changes

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -16,21 +16,18 @@ export class ProfileComponent implements OnInit {
   birthCheck: any;
   zipCheck: any;
   passwordCheck: any;
-  inputName: string | undefined;
-  inputDisplayName : string | undefined
-  inputEmail: string | undefined
-  inputPhone: string | undefined
-  inputBirth: string | undefined
-  inputZIP: string | undefined
-  inputPassword: string | undefined
-  inputPassword2: string | undefined
-  passWord: any;
+  userName: string | undefined;
+  displayName : string | undefined
+  emailId: string | undefined
+  contactNumber: string | undefined
+  dateOfBirth: string | undefined
+  zipCode: string | undefined
+  Password: string | undefined
+  confirmPassword: string | undefined
   phone: any;
   email: any;
-  displayName: any;
   DOB: any;
   ziper: any;
-  userName: any;
   img: File | undefined;
   imgUrl: string | undefined;
   // fileToUpload: File | null = null;
@@ -51,7 +48,7 @@ export class ProfileComponent implements OnInit {
       this.phone = <string>localStorage?.getItem('phone');
       this.ziper = <string>localStorage?.getItem('ZIP');
       this.displayName = <string>localStorage?.getItem('displayName');
-      this.passWord = <string>localStorage?.getItem(('password'));
+    //  this.Password = <string>localStorage?.getItem(('password'));
       this.email = <string>localStorage?.getItem(('email'));
 
   }
@@ -61,7 +58,7 @@ export class ProfileComponent implements OnInit {
   }
 
   onSubmit2() {
-    var namePattern = new RegExp("[a-zA-Z]+[a-zA-Z0-9]*");
+var namePattern =  new RegExp("^[A-Za-z]\\w{3,29}$");
     var emailPattern = new RegExp(".@.")
     var zipPattern = new RegExp("[0-9]{5}")
     var phonePattern = new RegExp("[0-9]{3}-[0-9]{3}-[0-9]{4}")
@@ -71,94 +68,62 @@ export class ProfileComponent implements OnInit {
     var resultPhone;
     var today = new Date();
     var now = today.getTime();
-    var dob = document.getElementById("loginBirth")
+    var dob = document.getElementById("dateOfBirth");
     var ymd = (<HTMLInputElement>dob).value.split("-");
     var dobyear = parseInt(ymd[0]) + 18;
     var dobmonth = parseInt(ymd[1], 10);
     var dobday = parseInt(ymd[2], 10);
     var userDob = new Date();
     userDob.setFullYear(dobyear, dobmonth - 1, dobday);
+   if(typeof this.dateOfBirth === "string"){
+    this.birthCheck = "Please enter Date of Birth";
+   }
+      
     if (userDob.getTime() > now) {
       this.birthCheck = "User must older than 18 years old";
     }
     else {
       this.birthCheck = "";
     }
-    if (typeof this.inputName === "string")
-      resultName = namePattern.test(this.inputName);
+    if (typeof this.userName === "string" && this.userName != "")
+      resultName = namePattern.test(this.userName);
     if (!resultName)
-      this.nameCheck = "Expect proper format is a string of upper and lower case letters and numbers begin with a letter";
-    if (this.inputName && resultName)
+      this.nameCheck = "Please enter a valid value that should not start with digit and min length is 6";
+
+    if (this.userName && resultName)
       this.nameCheck = "";
-    if (typeof this.inputEmail === "string")
-      resultEmail = emailPattern.test(this.inputEmail);
+    if (typeof this.emailId === "string" && this.emailId != "")
+      resultEmail = emailPattern.test(this.emailId);
     if (!resultEmail)
-      this.emailCheck = "Expected email form should be include a @. Ex: rl88@rice.edu";
-    if (this.inputEmail && resultEmail)
+      this.emailCheck = "Invalid Email Address! Enter a valid Email Address of the form xxxxx@xxxxx.xxx";
+    if (this.emailId && resultEmail)
       this.emailCheck = "";
-    if (typeof this.inputZIP === "string")
-      resultZIP = zipPattern.test(this.inputZIP);
+    if (typeof this.zipCode === "string")
+      resultZIP = zipPattern.test(this.zipCode);
     if (!resultZIP)
-      this.zipCheck = "Expected zip should be 5 digit";
-    if ((<HTMLInputElement>document.getElementById("loginZip")).value.length != 5)
-      this.zipCheck = "Expected zip should be 5 digit";
-    if (this.inputZIP && resultZIP && this.inputZIP.length == 5)
+      this.zipCheck = "Invalid Zipcode! Enter a 5-digit Zip Code.";
+    if ((<HTMLInputElement>document.getElementById("zipCode")).value.length != 5)
+      this.zipCheck = "Invalid Zipcode! Enter a 5-digit Zip Code.";
+    if (this.zipCode && resultZIP && this.zipCode.length == 5)
       this.zipCheck = "";
-    if (typeof this.inputPhone === "string")
-      resultPhone = phonePattern.test(this.inputPhone);
+    if (typeof this.contactNumber === "string")
+      resultPhone = phonePattern.test(this.contactNumber);
     if (!resultPhone)
-      this.phoneCheck = "Expected phone number format 123-123-1234";
-    if (this.inputPhone && resultPhone)
+      this.phoneCheck = "Invalid Phone Number! Enter a 10 digit phone number in the form 123-123-1234";
+    if (this.contactNumber && resultPhone)
       this.phoneCheck = "";
-    if ((<HTMLInputElement>document.getElementById("loginPassword")).value.length == 0)
+    if ((<HTMLInputElement>document.getElementById("password")).value.length == 0)
       this.passwordCheck = "The password cannot be empty";
-    else if (this.inputPassword == this.inputPassword2)
+    else if (this.Password == this.confirmPassword)
       this.passwordCheck="";
     else
-      this.passwordCheck = "The password do not matched";
-
+      this.passwordCheck = "Passwords do not match!";
     if (this.birthCheck == "" &&
       this.nameCheck == "" &&
       this.emailCheck == "" &&
       this.zipCheck == "" &&
       this.phoneCheck == "" &&
       this.passwordCheck=="") {
-      var userInfoKey = "user:" + this.inputName + ":" + this.inputPassword;
-      var data = {
-        "username": this.inputName,
-        "displayName": this.inputDisplayName,
-        "password": this.inputPassword,
-        "phone": this.inputPhone,
-        "ZIP": this.inputZIP,
-        "DOB": this.inputBirth,
-        "email": this.inputEmail
-      }
-      localStorage.setItem(userInfoKey, JSON.stringify(data));
-      var data2 = JSON.parse(<string>localStorage.getItem(userInfoKey));
-      localStorage.removeItem(userInfoKey);
-      this.userName = this.inputName;
-      this.DOB = this.inputBirth;
-      this.phone = this.inputPhone;
-      this.ziper = this.inputZIP;
-      this.displayName = this.inputDisplayName;
-      var len = (<HTMLInputElement>document.getElementById("loginPassword")).value.length;
-      var passwordTemp = '*';
-      var password = "";
-      while(len>0){
-        len--;
-        password = password + passwordTemp;
-      }
-      this.passWord = password;
-      this.email = this.inputEmail;
-      this.inputDisplayName="";
-      this.inputEmail="";
-      this.inputZIP="";
-      this.inputName="";
-      this.inputPhone="";
-      this.inputPassword="";
-      this.inputDisplayName="";
-      this.inputPassword2="";
-
       this.router.navigate(['profile']);
     }
   }
