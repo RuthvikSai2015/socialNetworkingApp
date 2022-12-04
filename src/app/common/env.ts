d5e6981bb3513e00b0a665d21be0bbd128d50382
RuthvikSai2015 <84248537+RuthvikSai2015@users.noreply.github.com>
2022-12-04 22:50:18 +0530

    front-end

--- src/app/common/env.ts
+++ src/app/common/env.ts
@@ -1,2 +1,2 @@
-export const url= "https://sn62backendhw8version5.herokuapp.com/";
+export const url= "https://sn62backendhw8version105.herokuapp.com/";
 //export const url= "http://localhost:3000/";
